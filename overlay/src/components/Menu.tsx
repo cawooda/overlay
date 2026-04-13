a6ffc7029caf439d04911145c584b547e147ab8e
Andrew Cawood <cawooda@gmail.com>
2026-04-13 20:35:57 +1000

    Initial commit

---
+++ overlay/src/components/Menu.tsx
@@ -0,0 +1,15 @@
+const menuStyle = {
+  padding: "16px",
+  position: "fixed",
+  bottom: "16px",
+  right: "16px",
+  color: "white",
+  backgroundColor: "orange",
+  borderRadius: "100%",
+};
+
+function Menu() {
+  return <div style={{ ...menuStyle }}>F</div>;
+}
+
+export default Menu;
